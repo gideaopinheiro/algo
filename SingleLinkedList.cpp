896a8490f22374d76313697121997cb0fc0dde20
gideaopinheiro <gideaosilva@vipcommerce.com.br>
2022-04-23 11:22:01 -0300

    single linked list

---
+++ SingleLinkedList.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Item {
+public:
+  int value;
+  Item *next;
+
+  friend class LinkedList;
+};
+
+class LinkedList {
+public:
+  Item *head;
+  void pushInFront(int num);
+  int pop();
+  void print();
+  LinkedList();
+  ~LinkedList();
+};
+
+LinkedList::LinkedList() : head(NULL) {}
+
+LinkedList::~LinkedList() {
+  while (head != NULL) {
+    pop();
+  }
+};
+
+void LinkedList::print() {
+  Item *temp = head;
+  while (temp->next != NULL) {
+    cout << "[ " << temp->value << " ] -> ";
+    temp = temp->next;
+  }
+  cout << "[ " << temp->value << " ]" << endl;
+}
+
+void LinkedList::pushInFront(int num) {
+  Item *node = new Item;
+  node->value = num;
+  node->next = head;
+  head = node;
+};
+
+int LinkedList::pop() {
+  int value = head->value;
+  Item *temp = head;
+  head = head->next;
+  temp->next = NULL;
+  delete temp;
+  return value;
+};
+
+int main(int argc, char *argv[]) {
+  LinkedList *ll = new LinkedList;
+
+  ll->pushInFront(2);
+  ll->print();
+  ll->pushInFront(5);
+  ll->print();
+  ll->pushInFront(8);
+  ll->print();
+  ll->pop();
+  ll->print();
+  ll->pushInFront(90);
+  ll->print();
+  ll->pushInFront(1);
+  ll->print();
+
+  return 0;
+}
